93719e408fe0feddae0ee8eeefdfd17c67d7a11c
malisahin <mehmetalisahinogullari@gmail.com>
2022-12-08 21:06:45 +0300

    Open Api Docs

---
+++ src/main/java/com/malibu/pwc/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.malibu.pwc.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Country Route Project")
+                                .version("1.0.0")
+                );
+    }
+
+    @Bean
+    public GroupedOpenApi httpApi() {
+        return GroupedOpenApi.builder()
+                .group("Api")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
